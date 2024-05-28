080574329acac56b2817f7fa351cabc48c22a0a4
Karthik Surabhi <kartheeksurabhi@gmail.com>
2024-05-28 02:28:21 -0500

    pass stage2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -23,14 +23,14 @@ public class Main {
       InputStream input = clientSocket.getInputStream();
       BufferedReader reader = new BufferedReader(new InputStreamReader(input));
       String line = reader.readLine();
-      String[] HttpRequest = line.split(" ",0);
+      String[] HttpRequest = line.split(" ");
       OutputStream output = clientSocket.getOutputStream();
       String[] str = HttpRequest[1].split("/");
       //System.out.println(HttpRequest[1]);
       if(HttpRequest[1].equals("/")){
         System.out.println("HTTP/1.1 200 OK\r\n\r\n");
       }
-      if((str.length > 2 && str[1].equals("echo"))){
+      else if((str.length > 2 && str[1].equals("echo"))){
         String responsebody = str[2];
         String finalstr = "HTTP/1.1 200 OK\r\n" + "Content-Type: text/plain\r\n"  + "Content-Length: " + responsebody.length() + "\r\n\r\n" + responsebody; 
           output.write(finalstr.getBytes());
