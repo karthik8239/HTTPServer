cdb795b7f27cadbe06e72924362fb7cf7744182d
Karthik Surabhi <kartheeksurabhi@gmail.com>
2024-05-28 22:21:17 -0500

    pass stage2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -34,6 +34,13 @@ public class Main {
         "Content-Length: 0\r\n\r\n";
         output.write(response.getBytes());
       }
+      else if(str[1].equals("useragent")){
+          reader.readLine();
+        String useragent = reader.readLine().split("\\s+")[1];
+        String reply = String.format("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %s\r\n\r\n%s\r\n",
+        useragent.length(), useragent);
+        output.write(reply.getBytes());
+      }
       else if((str.length > 2 && str[1].equals("echo"))){
         String responsebody = str[2];
         String finalstr = "HTTP/1.1 200 OK\r\n" + "Content-Type: text/plain\r\n"  + "Content-Length: " + responsebody.length() + "\r\n\r\n" + responsebody; 
